df42429cffd65380dc1050ff48415e94aff2ecb8
unknown <parthasarathibodapati@gmail.com>
2026-06-12 13:52:19 +0530

    Fixed indentation issues

--- student_performance_prediction/app.py
+++ student_performance_prediction/app.py
@@ -1,24 +1,34 @@
 import streamlit as st
 import pickle
 import numpy as np
+from pathlib import Path
 
-# Load trained model
-with open("model.pkl", "rb") as file:
+# Get current folder path
+
+BASE_DIR = Path(__file__).parent
+
+# Load model
+
+model_path = BASE_DIR / "model.pkl"
+
+with open(model_path, "rb") as file:
     model = pickle.load(file)
 
 # Title
+
 st.title("Student Performance Predictor")
 
 st.write("Enter student details below:")
 
-# User Inputs
+# Inputs
+
 hours = st.slider("Study Hours", 0, 12, 5)
 
 attendance = st.slider("Attendance Percentage", 0, 100, 75)
 
-# Predict Button
-if st.button("Predict Result"):
+# Prediction
 
+if st.button("Predict Result"):
     input_data = np.array([[hours, attendance]])
 
     prediction = model.predict(input_data)
@@ -27,3 +37,4 @@ if st.button("Predict Result"):
         st.success("Student Will PASS")
     else:
         st.error("Student Will FAIL")
+
